7de1727f82dc71d879ccb49caae96eb4def267fd
cwang87 <cwang87@usfca.edu>
2018-03-20 14:57:13 -0700

    allow users to provide filename when file_logger is instantiated

--- Lab1/file_logger.py
+++ Lab1/file_logger.py
@@ -2,8 +2,13 @@ from logger import logger
 
 class file_logger (logger):
 
+	def __init__(self, log_level, file_name="file_log.txt"):
+		logger.__init__(self, log_level)
+		self.__file_name__ = file_name
+
 	def log(self, log_level, message) :
 		if (log_level <= self.__log_level__):
-			my_file = open("file_log.txt", "a")
+			my_file = open(self.__file_name__, "a")
 			my_file.write("{}: {}\n".format(log_level, message))
 			my_file.close()
+
